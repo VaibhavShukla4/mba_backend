344ad69aecb05c1d49e40e90a04f76e74262afca
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2023-06-23 13:51:25 +0530

    deploy-&-connect-to-mongoose

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    DB_NAME: "mba-db",
-    DB_URL: "mongodb+srv://admin:admin@mba-db.1rx1y.mongodb.net/?retryWrites=true&w=majority"
-
-}
-
+  DB_NAME: "mba-db",
+  DB_URL:
+    "mongodb+srv://vaibhavs7312:JzSh3Ypq0lkNvqPd@cluster0.bolrvgc.mongodb.net/",
+};
 
 // DB_URL: "mongodb+srv://admin:admin@mba-db.1rx1y.mongodb.net/?retryWrites=true&w=majority"
